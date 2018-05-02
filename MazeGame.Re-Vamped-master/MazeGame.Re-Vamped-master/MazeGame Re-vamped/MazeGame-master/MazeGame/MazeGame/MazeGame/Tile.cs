fa5b15fe894f79d7a5c7e077e0e4d0c495fd0be7
theasianballer <38295885+theasianballer@users.noreply.github.com>
2018-05-02 13:42:53 -0500

    Add files via upload

---
+++ MazeGame.Re-Vamped-master/MazeGame.Re-Vamped-master/MazeGame Re-vamped/MazeGame-master/MazeGame/MazeGame/MazeGame/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MazeGame
+{
+    enum TileType
+    {
+        Floor, Wall, Door
+    }
+    class Tile 
+    {
+        public Rectangle TileRect;
+        public Texture2D TileTexture;
+        public Color TileColor;
+        public TileType Tiletype;
+
+        public Tile(Rectangle TileR, Texture2D TileT, Color TileC, TileType type)
+        {
+            TileRect = TileR;
+            TileTexture = TileT;
+            TileColor = TileC;
+            Tiletype = type;
+        }
+    }
+}
