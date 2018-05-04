50075b1e09ab08695f016006f18a36a2a2c785ea
Scal4 <34249624+Scal4@users.noreply.github.com>
2018-05-04 08:04:54 -0500

    Add files via upload

---
+++ MazeGame/MazeGame/MazeGame/Audio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+namespace MazeGame
+{
+    class Audio
+    {
+        List<Song> songs = new List<Song>();
+        GameState gameState;
+
+        public Audio(List<Song> songs, GameState gameState)
+        {
+            this.songs = songs;
+            this.gameState = gameState;
+        }
+    }
+}
---
+++ MazeGame/MazeGame/MazeGame/Tile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MazeGame
+{
+    enum TileType
+    {
+        Floor, Wall, Door, AdjacentDoorTiles
+    }
+    class Tile
+    {
+        public Rectangle TileRect;
+        public Texture2D TileTexture;
+        public Color TileColor;
+        public TileType Tiletype;
+        public int TileDrawLayer;
+        public int TileTimer;
+
+        public Tile(Rectangle TileR, Texture2D TileT, Color TileC, TileType type, int TileDrawL)
+        {
+            TileRect = TileR;
+            TileTexture = TileT;
+            TileColor = TileC;
+            Tiletype = type;
+            TileDrawLayer = TileDrawL;
+            TileTimer = 0;
+            CreateDoorTimer(type);
+
+        }
+
+        private void CreateDoorTimer(TileType type)
+        {
+            if(type == TileType.Door)
+            {
+                TileTimer = 240; // 4 secs, 2 for open 2 for closed.
+            }
+        }
+    }
+}
